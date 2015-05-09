f86e83cbf571539a1f77e7a2190a4153b5524966
Martin Elff <elff@gmx.com>
2015-05-09 19:42:06 +0200

    2015-05-09: Codebook support for numeric vectors and factors

--- pkg/R/codebook-methods.R
+++ pkg/R/codebook-methods.R
@@ -10,6 +10,25 @@ setMethod("codebook","item",function(x){
   new("codebook",cb)
 })
 
+setMethod("codebook","data.frame",function(x){
+  cb <- lapply(x,codebookEntry)
+  new("codebook",cb)
+})
+
+setMethod("codebook","atomic",function(x){
+  xname <- paste(deparse(substitute(x)))
+  cb <- list(codebookEntry(x))
+  names(cb) <- xname
+  new("codebook",cb)
+})
+
+setMethod("codebook","factor",function(x){
+  xname <- paste(deparse(substitute(x)))
+  cb <- list(codebookEntry(x))
+  names(cb) <- xname
+  new("codebook",cb)
+})
+
 setMethod("codebookEntry","item",function(x){
   annotation <- annotation(x)
   filter <- x@value.filter
@@ -32,6 +51,60 @@ setMethod("codebookEntry","item",function(x){
     annotation = annotation
   )
 })
+
+setMethod("codebookEntry","atomic",function(x){
+  spec <- c(
+            "Storage mode:"=storage.mode(x)
+            )
+  isna <- is.na(x)
+  stats <- summary(x)
+  stats <- list(descr=c(stats))
+  new("codebookEntry",
+    spec = spec,
+    stats = stats,
+    annotation = NULL
+  )
+})
+setMethod("codebookEntry","factor",function(x){
+
+  spec <- c("Storage mode:"=storage.mode(x))
+  spec <- if(is.ordered(x)) c(spec,
+            "Ordered factor with"=paste(nlevels(x),"levels"))
+          else c(spec,
+            "Factor with"=paste(nlevels(x),"levels"))
+            
+  isna <- is.na(x)
+  NAs <- sum(isna)
+
+  counts <- table(x)
+  tab <- cbind(counts,100*counts/sum(counts))
+  if(any(isna)) {
+      labs <- sQuote(levels(x))
+      labs <- paste(format(c(1:nlevels(x),NA),justify="right"),
+                    format(c(labs,""),justify="left")
+                    )
+      tab <- rbind(tab,c(NAs,NA))
+      counts <- c(counts,NAs)
+      tab <- cbind(tab,100*counts/sum(counts))
+    }
+  else {
+      labs <- sQuote(levels(x))
+      labs <- paste(format(1:nlevels(x),justify="right"),
+                    format(labs,justify="left")
+                    )
+  }
+  rownames(tab) <- labs
+  
+  names(dimnames(tab))[1] <- "Levels"
+  stats <- list(tab=tab)
+  stats <- c(stats,list(NAs = NAs))
+  new("codebookEntry",
+    spec = spec,
+    stats = stats,
+    annotation = NULL
+  )
+})
+
 
 codebookStatsCateg <- function(x)
     list(tab=Table(x,style="codebook"))
