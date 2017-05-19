a8fbd599c8ff182154190de62d62add5e74c246b
Martin Elff <memisc@elff.eu>
2017-05-19 21:59:58 +0200

    Add methods for `repr` generic - mtable, ftable, ftable_matrix now have HTML output in Jupyter

---
+++ pkg/R/repr-methods.R
@@ -0,0 +1,5 @@
+repr_html.mtable <- function(obj) mtable_format_html(obj)
+
+repr_html.ftable <- function(obj) format_html.ftable(obj)
+
+repr_html.ftable_matrix <- function(obj) format_html.ftable_matrix(obj)
