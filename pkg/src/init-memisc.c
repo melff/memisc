14fad4d71bd9f30257153328d338cf64fe73a175
Martin Elff <memisc@elff.eu>
2017-12-19 14:36:04 +0100

    Fix compilation problems with R version < 3.4

--- pkg/src/init-memisc.c
+++ pkg/src/init-memisc.c
@@ -1,9 +1,18 @@
 #include "memisc.h"
-#include <R_ext/Visibility.h>
 #include <Rversion.h>
 
 #if R_VERSION >= R_Version(3,4,0)
+#include <R_ext/Visibility.h>
+#include <R_ext/Rdynload.h>
 void attribute_visible R_init_memisc(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallMethods, NULL, NULL);
+		R_useDynamicSymbols(dll, FALSE);
+		
+}
+#else
+#include <R_ext/Rdynload.h>
+void R_init_memisc(DllInfo *dll)
 {
     R_registerRoutines(dll, NULL, CallMethods, NULL, NULL);
 		R_useDynamicSymbols(dll, FALSE);
