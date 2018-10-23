f001b13536c3425650f382b562c84e04e441bbf6
Martin Elff <memisc@elff.eu>
2018-10-23 22:33:41 +0200

    Handle visibility of binary code symbols correctly for R releases 3.3.0 through 3.3.3

--- pkg/src/init-memisc.c
+++ pkg/src/init-memisc.c
@@ -1,7 +1,6 @@
 #include "memisc.h"
 #include <Rversion.h>
 
-#if R_VERSION >= R_Version(3,4,0)
 #include <R_ext/Visibility.h>
 #include <R_ext/Rdynload.h>
 void attribute_visible R_init_memisc(DllInfo *dll)
@@ -10,12 +9,3 @@ void attribute_visible R_init_memisc(DllInfo *dll)
 		R_useDynamicSymbols(dll, FALSE);
 		
 }
-#else
-#include <R_ext/Rdynload.h>
-void R_init_memisc(DllInfo *dll)
-{
-    R_registerRoutines(dll, NULL, CallMethods, NULL, NULL);
-		R_useDynamicSymbols(dll, FALSE);
-		
-}
-#endif
